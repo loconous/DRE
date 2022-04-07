5e2835322799e1c62d8bef452f6cbb8bd0f15c1a
Gabriel Stowe <gstowe1@vols.utk.edu>
2022-04-06 21:13:47 -0400

    Reworked search form. Now has a autocomplete search box that current only show static examples in array. Next goal is to create backend to fetch files from folder and place into the array. #45

--- client/app/components/common/custom_inputbox.js
+++ client/app/components/common/custom_inputbox.js
@@ -1,29 +1,19 @@
 import React, { Component } from 'react';
-import Paper from '@mui/material/Paper';
-import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
 export default class Custom_InputBox extends Component {
  render()
  {
   return (
-    <Paper
-      sx={{display: 'flex', alignItems: 'center', width:"100%" }}
-    >
-
-      <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder={this.props.placeholder}
-        onChange={this.props.getValue}
-      />
-      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-
-      <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
-        <SearchIcon />
-      </IconButton>
-    </Paper>
+    <div>
+    <Autocomplete
+      onInputChange={this.props.onChange}
+      options={this.props.options}
+      sx={{flex:1 }}
+      renderInput={(params) => <TextField {...params} label="SHA" />}
+    />
+  </div>
   );
  }
 }
