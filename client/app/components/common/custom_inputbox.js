52df14bc6859350f7418e4cade678de7484e252d
Gabriel Stowe <gstowe1@vols.utk.edu>
2022-05-04 17:10:26 -0400

    Texted entered in text box will not clear once clicking off the text box. Fixed #96

--- client/app/components/common/custom_inputbox.js
+++ client/app/components/common/custom_inputbox.js
@@ -6,14 +6,17 @@ export default class Custom_InputBox extends Component {
  render()
  {
   return (
-    <div>
-    <Autocomplete
-      onInputChange={this.props.onChange}
-      options={this.props.options}
-      sx={{flex:1 }}
-      renderInput={(params) => <TextField {...params} label="SHA" />}
-    />
-  </div>
+      <div>
+      <Autocomplete
+        freeSolo
+        disableClearable
+        onInputChange={this.props.onChange}
+        onInputChange={this.props.onInputChange}
+        options={this.props.options}
+        sx={{flex:1 }}
+        renderInput={(params) => <TextField {...params} label="SHA" />}
+      />
+      </div>
   );
  }
 }
