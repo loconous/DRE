abe659155e143cf4c2568799d17ecbcdfb98d4cb
Trevor <mangrumtrevor@gmail.com>
2022-05-04 14:46:03 -0400

    Fixes an input bug in vulnerability search form & cleans up frontend

--- client/app/components/common/custom_inputbox.js
+++ client/app/components/common/custom_inputbox.js
@@ -2,18 +2,39 @@ import React, { Component } from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
-export default class Custom_InputBox extends Component {
- render()
- {
+const CustomInputBox = (props) => {
   return (
     <div>
-    <Autocomplete
-      onInputChange={this.props.onChange}
-      options={this.props.options}
-      sx={{flex:1 }}
-      renderInput={(params) => <TextField {...params} label="SHA" />}
-    />
+      <Autocomplete
+        onInputChange={props.onInputChange}
+        options={props.options}
+        value={props.value}
+        isOptionEqualToValue={(option, value) => option.value == value.value}
+        inputValue={props.value}
+        sx={{flex:1 }}
+        renderInput={(params) => <TextField {...params} label="SHA" />}
+      />
   </div>
-  );
- }
+  )
 }
+
+export default CustomInputBox;
+
+// export default class Custom_InputBox extends Component {
+//  render()
+//  {
+//   return (
+//     <div>
+//     <Autocomplete
+//       onInputChange={this.props.onInputChange}
+//       options={this.props.options}
+//       value={this.props.value}
+//       isOptionEqualToValue={(option, value) => option.value == value.value}
+//       inputValue={this.props.value}
+//       sx={{flex:1 }}
+//       renderInput={(params) => <TextField {...params} label="SHA" />}
+//     />
+//   </div>
+//   );
+//  }
+// }
